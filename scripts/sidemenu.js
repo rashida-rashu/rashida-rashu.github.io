34187c8c63641ec0317ad76a9808a3a4ce6927aa
twily-chan <raiyaadraad132rafan45@gmail.com>
2025-06-27 12:13:44 +0600

    Fixed

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -1,18 +1,31 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var sidemenu = document.getElementById('sidemenu');
+
+//     function openmenu() {
+//         sidemenu.style.right = "0";
+//     }
+
+//     function closemenu() {
+//         sidemenu.style.right = "-200px";
+//     }
+
+//     document.querySelector('.fa-bars').addEventListener('click', function() {
+//         openmenu();
+//     });
+//     document.querySelector('.fa-times').addEventListener('click', function() {
+//         closemenu();
+//     });
+// });
 document.addEventListener('DOMContentLoaded', function() {
-    var sidemenu = document.getElementById('sidemenu');
+    const sidemenu = document.getElementById('sidemenu');
+    const openBtn = document.querySelector('.fa-bars');
+    const closeBtn = document.querySelector('.fa-times');
 
-    function openmenu() {
+    openBtn.addEventListener('click', function() {
         sidemenu.style.right = "0";
-    }
-
-    function closemenu() {
-        sidemenu.style.right = "-200px";
-    }
-
-    document.querySelector('.fa-bars').addEventListener('click', function() {
-        openmenu();
     });
-    document.querySelector('.fa-times').addEventListener('click', function() {
-        closemenu();
+
+    closeBtn.addEventListener('click', function() {
+        sidemenu.style.right = "-200px";
     });
 });
